3c20ac78c04d9aed338432b4d9be5ca3f9656365
MAG45 <igorfilonenko3@gmail.com>
2021-11-16 02:27:32 +0200

    Update main.js

--- access/main.js
+++ access/main.js
@@ -135,7 +135,7 @@ swiper.on('slideChange', function () {
 		$('.dit_dot:nth-child(' + sss + ')').addClass('act');
 
 		$('html').css('background', '#B3E3C3');
-		$('.content').css('background', '#EDE3EB');
+		$('.content').css('background', '#F5F4D5');
 	}
 
 	if (swiper.realIndex == 2) {
